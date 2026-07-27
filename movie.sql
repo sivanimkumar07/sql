6ca2ceff878c2ebc397308f595ff25c389108e03
sivani <sivanimkumar7@gmail.com>
2026-07-27 12:59:01 +0530

    create movie.sql to define movies table and manage movie records

---
+++ movie.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE smallmovie;
+CREATE TABLE movies(id  INT (100),
+title VARCHAR (100),
+genre  VARCHAR(100),
+release_year INT (100),
+rating  DECIMAL(10,2),
+box_office DECIMAL (11,2));
+INSERT INTO movies VALUES (1, 'Inception', 'Sci-Fi', 2010, 8.8, 825),
+(2, 'The Dark Knight', 'Action', 2008, 9.0, 1004),
+(3, 'Interstellar', 'Sci-Fi', 2014, 8.6, 677),
+(4, 'Joker', 'Drama', 2019, 8.4, 1074),
+(5, 'Dunkirk', 'War', 2017, 7.9, 527);
+SELECT title,
+box_office AS  Earnings
+FROM movies;
+SELECT title AS 'Movie Title',
+genre AS category
+FROM movies;
+SELECT *,
+       rating AS 'IMDb Score'
+FROM movies
+WHERE genre IN ('Sci-Fi', 'Action');
+SELECT *,
+       release_year AS 'Release'
+FROM movies
+WHERE release_year BETWEEN 2008 AND 2015;
+SELECT COUNT(*) AS 'High Rated Movies'
+FROM movies
+WHERE rating > 8.5;
